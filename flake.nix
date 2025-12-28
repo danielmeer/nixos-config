fd38cfe085969a6fecdf2ceead91caa583b9a42d
Daniel Meer <meerdan2@gmail.com>
2025-12-28 20:19:02 +0100

    Update to NixOS 25.11

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "A NixOS flake for my home server";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # home-manager, used for managing user configuration
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Atomic secret provisioning for NixOS based on sops
